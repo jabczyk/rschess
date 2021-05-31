1862ece61e7fc246bb1089e2cfc1e747eb69e6ec
Michal Jabczyk <jabczyk.m@gmail.com>
2021-05-31 19:31:48 +0200

    feat: get square index from rank and file

--- src/bitboard.rs
+++ src/bitboard.rs
@@ -1,15 +1,23 @@
+#[inline]
 pub fn get_bit(bitboard: u64, square: u8) -> u64 {
     bitboard & (1u64 << square)
 }
 
+#[inline]
 pub fn set_bit(bitboard: &mut u64, square: u8) {
     *bitboard |= 1u64 << square
 }
 
+#[inline]
 pub fn del_bit(bitboard: &mut u64, square: u8) {
     if get_bit(*bitboard, square) != 0 {
         *bitboard ^= 1u64 << square
     }
+}
+
+#[inline]
+pub fn square(rank: u8, file: u8) -> u8 {
+    rank * 8 + file
 }
 
 pub fn print_bitboard(bitboard: u64) {
@@ -18,8 +26,7 @@ pub fn print_bitboard(bitboard: u64) {
         print!("{} ", 8 - rank);
 
         for file in 0..8 {
-            let square = rank * 8 + file;
-            print!(" {}", if get_bit(bitboard, square) == 0 { 0 } else { 1 });
+            print!(" {}", if get_bit(bitboard, square(rank, file)) == 0 { 0 } else { 1 });
         }
 
         println!();
@@ -31,14 +38,14 @@ mod tests {
     use super::*;
 
     #[test]
-    fn it_gets_bit() {
+    fn gets_bit() {
         assert_eq!(get_bit(5u64, 0), 1);
         assert_eq!(get_bit(5u64, 1), 0);
         assert_eq!(get_bit(5u64, 2), 4);
     }
 
     #[test]
-    fn it_sets_bit() {
+    fn sets_bit() {
         let mut bitboard: u64 = 0;
         set_bit(&mut bitboard, 2);
         set_bit(&mut bitboard, 5);
@@ -49,11 +56,18 @@ mod tests {
     }
 
     #[test]
-    fn it_deletes_bit_if_necessary() {
+    fn deletes_bit_if_necessary() {
         let mut bitboard: u64 = 4;
         del_bit(&mut bitboard, 2);
         assert_eq!(get_bit(bitboard, 2), 0);
         del_bit(&mut bitboard, 2);
         assert_eq!(get_bit(bitboard, 2), 0);
+    }
+
+    #[test]
+    fn returns_square_index() {
+        assert_eq!(square(1, 0), 8);
+        assert_eq!(square(1, 2), 10);
+        assert_eq!(square(0, 7), 7);
     }
 }
