eb4106b774dc81a459d2dbe83134bd3d70c3e7d0
Michal Jabczyk <jabczyk.m@gmail.com>
2021-06-01 21:12:24 +0200

    test: add pawn attack table tests

--- src/pieces/pawn.rs
+++ src/pieces/pawn.rs
@@ -46,3 +46,45 @@ pub fn get_pawn_attacks_table() -> [[u64; 64]; 2] {
 
     table
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::enums::Square::*;
+    use crate::test::*;
+
+    mod get_attacks {
+        use super::*;
+
+        #[test]
+        fn generates_attacks() {
+            verify_bitboard(get_pawn_attacks(Side::White, E4 as u8), vec![D5, F5]);
+            verify_bitboard(get_pawn_attacks(Side::Black, E5 as u8), vec![D4, F4]);
+        }
+
+        #[test]
+        fn valid_a_h_files() {
+            verify_bitboard(get_pawn_attacks(Side::White, A2 as u8), vec![B3]);
+            verify_bitboard(get_pawn_attacks(Side::White, H2 as u8), vec![G3]);
+
+            verify_bitboard(get_pawn_attacks(Side::Black, A7 as u8), vec![B6]);
+            verify_bitboard(get_pawn_attacks(Side::Black, H7 as u8), vec![G6]);
+        }
+
+        #[test]
+        fn edges() {
+            verify_bitboard(get_pawn_attacks(Side::White, A8 as u8), vec![]);
+            verify_bitboard(get_pawn_attacks(Side::White, H8 as u8), vec![]);
+            verify_bitboard(get_pawn_attacks(Side::Black, A1 as u8), vec![]);
+            verify_bitboard(get_pawn_attacks(Side::Black, H1 as u8), vec![]);
+        }
+    }
+
+    #[test]
+    fn generates_table() {
+        let table = get_pawn_attacks_table();
+
+        verify_bitboard(table[Side::White as usize][E4 as usize], vec![D5, F5]);
+        verify_bitboard(table[Side::Black as usize][E5 as usize], vec![D4, F4]);
+    }
+}
