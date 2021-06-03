deb96e0492dd1ac3a1558656dd70afc5aaa77890
Michal Jabczyk <jabczyk.m@gmail.com>
2021-06-03 22:29:51 +0200

    test: add rook occupancy generation tests

--- src/pieces/rook.rs
+++ src/pieces/rook.rs
@@ -27,3 +27,54 @@ pub fn get_rook_occupancy(square: u8) -> u64 {
 
     bitboard
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::enums::Square::*;
+    use crate::test::*;
+
+    mod get_occupancy {
+        use super::*;
+
+        #[test]
+        fn generates_occupacy() {
+            verify_bitboard(
+                get_rook_occupancy(E4 as u8),
+                vec![E7, E6, E5, E3, E2, B4, C4, D4, F4, G4],
+            );
+        }
+
+        #[test]
+        fn valid_edge_a1() {
+            verify_bitboard(
+                get_rook_occupancy(A1 as u8),
+                vec![A7, A6, A5, A4, A3, A2, B1, C1, D1, E1, F1, G1],
+            );
+        }
+
+        #[test]
+        fn valid_edge_a8() {
+            verify_bitboard(
+                get_rook_occupancy(A8 as u8),
+                vec![B8, C8, D8, E8, F8, G8, A7, A6, A5, A4, A3, A2],
+            );
+        }
+
+        #[test]
+        fn valid_edge_h1() {
+            verify_bitboard(
+                get_rook_occupancy(H1 as u8),
+                vec![H7, H6, H5, H4, H3, H2, B1, C1, D1, E1, F1, G1],
+            );
+        }
+
+        #[test]
+        fn valid_edge_h8() {
+            verify_bitboard(
+                get_rook_occupancy(H8 as u8),
+                vec![B8, C8, D8, E8, F8, G8, H7, H6, H5, H4, H3, H2],
+            );
+        }
+    }
+}
