e54ba2fbd4e2a1192739e68fdfc33d3f38775a80
Michal Jabczyk <jabczyk.m@gmail.com>
2021-06-05 21:21:30 +0200

    test: add rook attack generation tests

--- src/pieces/rook.rs
+++ src/pieces/rook.rs
@@ -69,34 +69,44 @@ mod tests {
         }
 
         #[test]
-        fn valid_edge_a1() {
+        fn valid_edges() {
             verify_bitboard(
                 get_rook_occupancy(A1 as u8),
                 vec![A7, A6, A5, A4, A3, A2, B1, C1, D1, E1, F1, G1],
             );
-        }
-
-        #[test]
-        fn valid_edge_a8() {
             verify_bitboard(
                 get_rook_occupancy(A8 as u8),
                 vec![B8, C8, D8, E8, F8, G8, A7, A6, A5, A4, A3, A2],
             );
-        }
-
-        #[test]
-        fn valid_edge_h1() {
             verify_bitboard(
                 get_rook_occupancy(H1 as u8),
                 vec![H7, H6, H5, H4, H3, H2, B1, C1, D1, E1, F1, G1],
             );
-        }
-
-        #[test]
-        fn valid_edge_h8() {
             verify_bitboard(
                 get_rook_occupancy(H8 as u8),
                 vec![B8, C8, D8, E8, F8, G8, H7, H6, H5, H4, H3, H2],
+            );
+        }
+    }
+
+    mod get_attacks {
+        use super::*;
+
+        #[test]
+        fn generates_attacks() {
+            verify_bitboard(
+                get_rook_attacks(D4 as u8, 0),
+                vec![D8, D7, D6, D5, D3, D2, D1, A4, B4, C4, E4, F4, G4, H4],
+            );
+        }
+
+        #[test]
+        fn handles_blockers() {
+            let blockers = bitboard_with(vec![D6, A4, G4, D2]);
+
+            verify_bitboard(
+                get_rook_attacks(D4 as u8, blockers),
+                vec![D6, D5, D3, D2, A4, B4, C4, E4, F4, G4],
             );
         }
     }
